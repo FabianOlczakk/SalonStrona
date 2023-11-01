d79c72a9f565b5101c2ba2e2bd97bf5929419771
FabianOlczakk <fabianolczak22@gmail.com>
2023-11-01 04:35:51 +0100

    Foooter, price, gallery and contact added

--- gallery.js
+++ gallery.js
@@ -1,53 +1,57 @@
+const galleryGrid = document.getElementById('gallery-grid');
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+let currentImageIndex;
+const totalImages = 178;
 
-    const carousel = document.querySelector('.carousel-images');
-    const nextBtn = document.getElementById('nextBtn');
-    const prevBtn = document.getElementById('prevBtn');
-    let imageIndex = 1;
-    const totalImages = 4;
-
+function loadImages() {
     for (let i = 1; i <= totalImages; i++) {
         const img = document.createElement('img');
-        img.src = `img/gallery/${i}.jpeg`;
-        carousel.appendChild(img);
+        img.src = `img/gallery/${i}.jpg`;
+        img.onclick = function() {
+            openModal(i);
+        };
+        galleryGrid.appendChild(img);
     }
-    
-    showImage();    
+}
 
-    function showImage() {
-        const images = document.querySelectorAll('.carousel img');
-        images.forEach((img, index) => {
-            if (index === imageIndex - 1) {
-                img.classList.add('active');
-            } else {
-                img.classList.remove('active');
-            }
-        });
-    }                       
+function openModal(index) {
+    modal.style.display = 'block';
+    currentImageIndex = index;
+    modalImage.src = `img/gallery/${index}.jpg`;
+}
 
-    nextBtn.addEventListener('click', () => {
-        if (imageIndex < totalImages) {
-            imageIndex++;
-        } else {
-            imageIndex = 1;
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function changeSlide(step) {
+    currentImageIndex += step;
+    if (currentImageIndex < 1) {
+        currentImageIndex = totalImages;
+    } else if (currentImageIndex > totalImages) {
+        currentImageIndex = 1;
+    }
+    modalImage.src = `img/gallery/${currentImageIndex}.jpg`;
+}
+
+loadImages();
+
+modal.onclick = function(event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' && modal.style.display === 'block') {
+        closeModal();
+    }
+    if (modal.style.display === 'block') {
+        if (event.key === 'ArrowLeft') {
+            changeSlide(-1);
+        } else if (event.key === 'ArrowRight') {
+            changeSlide(1);
         }
-        showImage();
-    });
-    
-    prevBtn.addEventListener('click', () => {
-        if (imageIndex > 1) {
-            imageIndex--;
-        } else {
-            imageIndex = totalImages;
-        }
-        showImage();
-    });       
-
-    setInterval(() => {
-        if (imageIndex < totalImages) {
-            imageIndex++;
-        } else {
-            imageIndex = 1;
-        }
-        showImage();
-    }, 5000);
-
+    }
+});
---
+++ photos-change.js
@@ -0,0 +1,53 @@
+
+    const carousel = document.querySelector('.carousel-images');
+    const nextBtn = document.getElementById('nextBtn');
+    const prevBtn = document.getElementById('prevBtn');
+    let imageIndex = 1;
+    const totalImages = 178;
+
+    for (let i = 1; i <= totalImages; i++) {
+        const img = document.createElement('img');
+        img.src = `img/gallery/${i}.jpg`;
+        carousel.appendChild(img);
+    }
+    
+    showImage();    
+
+    function showImage() {
+        const images = document.querySelectorAll('.carousel img');
+        images.forEach((img, index) => {
+            if (index === imageIndex - 1) {
+                img.classList.add('active');
+            } else {
+                img.classList.remove('active');
+            }
+        });
+    }                       
+
+    nextBtn.addEventListener('click', () => {
+        if (imageIndex < totalImages) {
+            imageIndex++;
+        } else {
+            imageIndex = 1;
+        }
+        showImage();
+    });
+    
+    prevBtn.addEventListener('click', () => {
+        if (imageIndex > 1) {
+            imageIndex--;
+        } else {
+            imageIndex = totalImages;
+        }
+        showImage();
+    });       
+
+    setInterval(() => {
+        if (imageIndex < totalImages) {
+            imageIndex++;
+        } else {
+            imageIndex = 1;
+        }
+        showImage();
+    }, 3000);
+
